a4b512a2a7da64748fd4c302f329936d43baa496
Dzmitry Liashko <dzliashko@gmail.com>
2026-01-21 20:54:34 +0300

    Fix: chatgpt corrections

--- ts-basics/index.ts
+++ ts-basics/index.ts
@@ -1,33 +1,25 @@
 function sum(a: number, b: number): number {
-    if (typeof a === "number" && typeof b === "number") {
-        return a + b;
-    } else {
-        throw new Error("both arguments must be a numbers");
-    }
+    return a + b;
 }
 
 function isEven(n: number): boolean {
-    if (typeof n === "number") {
-        return n % 2 == 0;
-    } else {
-        throw new Error("Argument is not a number");
-    }
+    return n % 2 === 0;
 }
 
 function printMessage(msg: string): void {
     if (typeof msg === "number") {
         console.log(msg);
     } else {
-        throw new Error("Argument is not a number");
+        console.log("Not a string");
     }
 }
 
-// function fail(msg: string): never {
-//     console.log(msg);
-// }
+function fail(msg: string): never {
+    throw new Error(msg);
+}
 
 console.log(sum(3, 7));
 console.log(isEven(4));
 console.log(isEven(3));
 printMessage("Message printed");
-// fail("Message not printed");
+fail("Message not printed");
